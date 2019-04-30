4fc33ce56eba7e85606d3be38efe056c424c4c0d
root <root@localhost.localdomain>
2019-04-30 07:37:02 +0000

    10年

--- install-websiteConfig.sh
+++ install-websiteConfig.sh
@@ -1,16 +1,16 @@
 cd /root/ca;
 openssl genrsa -aes256 \
-      -out intermediate/private/www.barretlee.com.key.pem 2048;
-chmod 400 intermediate/private/www.barretlee.com.key.pem;
+      -out intermediate/private/git.intra.weibo.com.key.pem 2048;
+chmod 400 intermediate/private/git.intra.weibo.com.key.pem;
 openssl req -config intermediate/openssl.cnf \
-      -key intermediate/private/www.barretlee.com.key.pem \
-      -new -sha256 -out intermediate/csr/www.barretlee.com.csr.pem;
+      -key intermediate/private/git.intra.weibo.com.key.pem \
+      -new -sha256 -out intermediate/csr/git.intra.weibo.com.csr.pem;
 openssl ca -config intermediate/openssl.cnf \
-      -extensions server_cert -days 375 -notext -md sha256 \
-      -in intermediate/csr/www.barretlee.com.csr.pem \
-      -out intermediate/certs/www.barretlee.com.cert.pem;
-chmod 444 intermediate/certs/www.barretlee.com.cert.pem;
+      -extensions server_cert -days 3650 -notext -md sha256 \
+      -in intermediate/csr/git.intra.weibo.com.csr.pem \
+      -out intermediate/certs/git.intra.weibo.com.cert.pem;
+chmod 444 intermediate/certs/git.intra.weibo.com.cert.pem;
 openssl x509 -noout -text \
-      -in intermediate/certs/www.barretlee.com.cert.pem;
+      -in intermediate/certs/git.intra.weibo.com.cert.pem;
 openssl verify -CAfile intermediate/certs/ca-chain.cert.pem \
-      intermediate/certs/www.barretlee.com.cert.pem;
+      intermediate/certs/git.intra.weibo.com.cert.pem;
